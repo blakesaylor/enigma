3064b5be73e155d94d5dbf2a9809da51489c21e7
Blake Saylor <blakesaylor@gmail.com>
2022-06-11 13:52:08 -0600

    Create test for generating a random key string

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -21,4 +21,12 @@ RSpec.describe Enigma do
       expect(@enigma.message).to eq 'hello world end'
     end
   end
+
+  describe '#generate_random_key_string' do
+    it 'can generate a random five character key string with leading zeroes' do
+      key = @enigma.generate_random_key_string
+      expect(key).to be_a String
+      expect(key.length).to eq 5
+    end
+  end
 end
