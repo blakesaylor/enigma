40d3f00c4d2e0d09776545d04520b91d4f26bcf9
Blake Saylor <blakesaylor@gmail.com>
2022-06-11 14:16:27 -0600

    Create test for generating a hash of keys

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -29,4 +29,13 @@ RSpec.describe Enigma do
       expect(key.length).to eq 5
     end
   end
+
+  describe '#generate_keys_hash' do
+    it 'can generate a hash or A through D keys' do
+      key = '02715'
+      key_hash = @enigma.generate_keys_hash(key)
+      expect(key_hash.keys).to eq [:a_key, :b_key, :c_key, :d_key]
+      expect(key_hash.values).to eq [02, 27, 71, 15]
+    end
+  end
 end
