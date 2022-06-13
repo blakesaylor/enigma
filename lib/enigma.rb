63f684926fee2c36f7403cf15a8f7351031e5555
Blake Saylor <blakesaylor@gmail.com>
2022-06-13 12:16:22 -0600

    Create encrypt and decrypt methods that output to a file

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,7 +8,6 @@ class Enigma
 
   def initialize
     @message = ''
-    # @character_list = ("a".."z").to_a << " "
   end
 
   def parse_message(filename)
@@ -35,8 +34,28 @@ class Enigma
     end
     message
   end
-  #
-  # def encrypt(message, key, date)
-  #
-  # end
+
+  def encrypt(message, key, date)
+    keys = generate_keys_hash(key)
+    offsets = generate_offset_keys_hash_from_date(date)
+    shifts = generate_shifts_hash(keys, offsets)
+    encrypted_message = encrypt_message(shifts, message)
+    encrypted_file = File.open('encrypted.txt', 'w')
+    encrypted_file.write(encrypted_message)
+    encrypted_file.close
+    encrypted_output_hash = { encryption: encrypted_message, key: key, date: date }
+    puts encrypted_output_hash
+  end
+
+  def decrypt(message, key, date)
+    keys = generate_keys_hash(key)
+    offsets = generate_offset_keys_hash_from_date(date)
+    shifts = generate_shifts_hash(keys, offsets)
+    decrypted_message = decrypt_message(shifts, message)
+    decrypted_file = File.open('decrypted.txt', 'w')
+    decrypted_file.write(decrypted_message)
+    decrypted_file.close
+    decrypted_output_hash = { decryption: decrypted_message, key: key, date: date }
+    puts decrypted_output_hash
+  end
 end
