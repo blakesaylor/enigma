1fd4baf5f90debdb8f3c794270fa79823c87bff7
Blake Saylor <blakesaylor@gmail.com>
2022-06-14 16:41:20 -0600

    Update to handle possible bad user input

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,12 +1,18 @@
 require './lib/enigma'
 
-enigma = Enigma.new
-input_filename = ARGV[0]
-output_filename = ARGV[1]
-key = ARGV[2]
-date = ARGV[3]
-message = enigma.parse_message(input_filename)
-decrypted_message = enigma.decrypt(message, key, date)
-enigma.write_message(decrypted_message[:decryption], output_filename)
+if ARGV.length < 4
+  puts 'Invalid entry. Please try again in the format of:'
+  puts 'ruby ./lib/decrypt.rb inputfile.txt outputfile.txt key date'
+  puts 'e.g. ruby ./lib/decrypt.rb encrypted.txt decrypted.txt 02715 040895'
+else
+  enigma = Enigma.new
+  input_filename = ARGV[0]
+  output_filename = ARGV[1]
+  key = ARGV[2]
+  date = ARGV[3]
+  message = enigma.parse_message(input_filename)
+  decrypted_message = enigma.decrypt(message, key, date)
+  enigma.write_message(decrypted_message[:decryption], output_filename)
 
-puts "Created #{output_filename} with the key #{key} and date #{date}."
+  puts "Created #{output_filename} with the key #{key} and date #{date}."
+end
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,12 +1,18 @@
 require './lib/enigma'
 
-enigma = Enigma.new
-input_filename = ARGV[0]
-output_filename = ARGV[1]
-key = enigma.generate_random_key_string
-date = enigma.generate_todays_date_string
-message = enigma.parse_message(input_filename)
-encrypted_message = enigma.encrypt(message, key, date)
-enigma.write_message(encrypted_message[:encryption], output_filename)
+if ARGV.length < 2
+  puts 'Invalid entry. Please try again in the format of:'
+  puts 'ruby ./lib/encrypt.rb inputfile.txt outputfile.txt'
+  puts 'e.g. ruby ./lib/decrypt.rb message.txt encrypted.txt'
+else
+  enigma = Enigma.new
+  input_filename = ARGV[0]
+  output_filename = ARGV[1]
+  key = enigma.generate_random_key_string
+  date = enigma.generate_todays_date_string
+  message = enigma.parse_message(input_filename)
+  encrypted_message = enigma.encrypt(message, key, date)
+  enigma.write_message(encrypted_message[:encryption], output_filename)
 
-puts "Created #{output_filename} with the key #{key} and date #{date}."
+  puts "Created #{output_filename} with the key #{key} and date #{date}."
+end
