29c66f17eca87597d00456b2e206be0b42ec7db6
Blake Saylor <blakesaylor@gmail.com>
2022-06-14 16:32:53 -0600

    Add lines to encrypt/decrypt based on filename

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -6,6 +6,7 @@ output_filename = ARGV[1]
 key = ARGV[2]
 date = ARGV[3]
 message = enigma.parse_message(input_filename)
-enigma.decrypt(message, key, date)
+decrypted_message = enigma.decrypt(message, key, date)
+enigma.write_message(decrypted_message[:decryption], output_filename)
 
 puts "Created #{output_filename} with the key #{key} and date #{date}."
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -6,6 +6,7 @@ output_filename = ARGV[1]
 key = enigma.generate_random_key_string
 date = enigma.generate_todays_date_string
 message = enigma.parse_message(input_filename)
-enigma.encrypt(message, key, date)
+encrypted_message = enigma.encrypt(message, key, date)
+enigma.write_message(encrypted_message[:encryption], output_filename)
 
 puts "Created #{output_filename} with the key #{key} and date #{date}."
